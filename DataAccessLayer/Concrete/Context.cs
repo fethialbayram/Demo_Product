003d0a21abaf8160de8cbfc3d985144115e7b732
Fethi Güralp Albayram <fethi.grlp@gmail.com>
2024-07-08 16:28:43 +0300

    DataAcessLayer interfaces created, entitiy frameworks added, migration created as mig2 and generic repository implemented

--- DataAccessLayer/Concrete/Context.cs
+++ DataAccessLayer/Concrete/Context.cs
@@ -17,8 +17,10 @@ namespace DataAccessLayer.Concrete
         }
 
         public DbSet<Product> Products { get; set; }
+
         public DbSet<Customer> Customers { get; set; }
 
         public DbSet<Category> Categories { get; set; }
     }
+
 }
