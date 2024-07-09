63f7018328870668b2bf332a2a1f3df7fba31172
Fethi Güralp Albayram <fethi.grlp@gmail.com>
2024-07-09 13:45:40 +0300

    Partial views created for UILayout.cshtml, ProductController added

--- Demo_Product/Controllers/ProductController.cs
+++ Demo_Product/Controllers/ProductController.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Demo_Product.Controllers
 {
     public class ProductController : Controller
     {
+        ProductManager productManager = new ProductManager(new EfProductDal());
         public IActionResult Index()
         {
             return View();
