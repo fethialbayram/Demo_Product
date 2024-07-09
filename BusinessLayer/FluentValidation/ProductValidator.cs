f46a06d6f513bcf0f5fd4da3e414ba3e50978cc0
Fethi Güralp Albayram <fethi.grlp@gmail.com>
2024-07-09 11:01:36 +0300

    Managers implemented for Categor, Customer and Product. Added some Validators to BusinessLayer

---
+++ BusinessLayer/FluentValidation/ProductValidator.cs
@@ -0,0 +1,23 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.FluentValidation
+{
+    public class ProductValidator : AbstractValidator<Product>
+    {
+        public ProductValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Ürün adını boş geçemezsiniz!");
+            RuleFor(x => x.Name).MinimumLength(3).WithMessage("Ürün adı en az üç karakter olmalıdır!");
+            RuleFor(x => x.Stock).NotEmpty().WithMessage("Stok sayısı boş geçilemez!");
+            RuleFor(x => x.Price).NotEmpty().WithMessage("Fiyat değeri boş geçilemez!");
+           
+
+        }
+    }
+}
