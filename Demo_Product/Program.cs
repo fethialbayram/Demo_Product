166546e6cfcc9846a4453dc8ae87aef74825d9e4
Fethi Güralp Albayram <fethi.grlp@gmail.com>
2024-07-11 14:40:51 +0300

    AppRole and AppUser entities created, UserRegisterViewModel for register page created and register operations added to RegisterController

--- Demo_Product/Program.cs
+++ Demo_Product/Program.cs
@@ -1,9 +1,15 @@
+using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<Context>();
+builder.Services.AddIdentity<AppUser, AppRole>().AddEntityFrameworkStores<Context>();
+builder.Services.AddControllersWithViews();
 builder.Services.AddControllersWithViews();
 
-var app = builder.Build();
+var app = builder.Build(); 
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
